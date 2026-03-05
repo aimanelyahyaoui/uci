56ca270b73cc36196552892d591c7786384b0af3
Aiman El Yahyaoui Lazaar <1749573@uab.cat>
2026-03-05 02:46:27 +0100

    Conexión completa: Kafka -> Python -> PostgreSQL

---
+++ Hospital UCI.session.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM historial_constantes
+ORDER BY timestamp DESC
+LIMIT 10;
--- consumidor-uci.py
+++ consumidor-uci.py
@@ -1,18 +1,41 @@
 import json
+import psycopg2 # <-- NUEVO: Librería de PostgreSQL
 from kafka import KafkaConsumer
 
-# 1. Nos conectamos a la "caja" de Kafka
+# 1. Conexión a la "caja" de la Base de Datos
+conexion_db = psycopg2.connect(
+    host="localhost",
+    database="hospital_uci",
+    user="admin",
+    password="adminpassword",
+    port="5432"
+)
+cursor = conexion_db.cursor()
+
+# 2. Creamos la tabla (Igual que en SQLite)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS historial_constantes (
+        id SERIAL PRIMARY KEY,
+        paciente_id VARCHAR(50),
+        ritmo_cardiaco INT,
+        timestamp TIMESTAMP
+    )
+""")
+conexion_db.commit()
+print("🗄️  Base de datos conectada y tabla lista.")
+
+# 3. Conexión a la "caja" de Kafka
 consumer = KafkaConsumer(
-    'sensores_uci', # El mismo canal (topic) donde escribe el generador
+    'sensores_uci',
     bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='latest', # Solo queremos leer los datos nuevos que lleguen
-    value_deserializer=lambda x: json.loads(x.decode('utf-8')) # Desempaquetamos el JSON
+    auto_offset_reset='latest',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-print("🩺 Monitor de la UCI encendido. Esperando constantes vitales...")
-print("-" * 50)
+print("🩺 Monitor encendido. Escuchando a Kafka y guardando en PostgreSQL...")
+print("-" * 60)
 
-# 2. Nos quedamos escuchando infinitamente
+# 4. El bucle infinito: Leer de Kafka -> Guardar en DB
 for mensaje in consumer:
     datos = mensaje.value
     
@@ -20,5 +43,11 @@ for mensaje in consumer:
     pulsaciones = datos['ritmo_cardiaco']
     hora = datos['timestamp']
     
-    # Imprimimos de forma bonita lo que va llegando
-    print(f"PACIENTE: {paciente} | PULSACIONES: {pulsaciones} bpm | HORA: {hora}")
+    # <-- NUEVO: Insertamos el dato en la tabla
+    cursor.execute(
+        "INSERT INTO historial_constantes (paciente_id, ritmo_cardiaco, timestamp) VALUES (%s, %s, %s)",
+        (paciente, pulsaciones, hora)
+    )
+    conexion_db.commit() # Guardamos los cambios definitivamente
+    
+    print(f"💾 Guardado en DB -> PACIENTE: {paciente} | PULSACIONES: {pulsaciones} bpm")
